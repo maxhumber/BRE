41e86ae64cba6c99fc3772de90d907d55037768a
Max Humber <max.humber@gmail.com>
2019-10-26 12:31:52 -0400

    📈

--- interaction_machine2.py
+++ interaction_machine2.py
@@ -2,22 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from scipy.sparse import csr_matrix
-
-from sklearn.metrics.pairwise import euclidean_distances
-
-df = pd.DataFrame([
-    [0, 5, 4, 0, 0, 0],
-    [0, 5, 5, 5, 0, 0],
-    [5, 0, 0, 5, 0, 0],
-    [0, 5, 4, 0, 0, 5],
-    [0, 0, 0, 5, 3, 5]
-])
-
-euclidean_distances(df)
-
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
-
 from lightfm import LightFM
 
 # Quick Data Prep
@@ -26,9 +12,8 @@ df = pd.read_csv('data/candy.csv')
 users = df['user'].value_counts()[df['user'].value_counts() >= 5].index
 df = df[df['user'].isin(users)]
 df['product'] = df['product'].str.replace('/reviews/', '')
-df
 
-#### WIP
+# setup interactions
 
 df=df.copy()
 ratings='stars'
@@ -47,37 +32,36 @@ i = item_encoder.fit_transform(_items)
 lu = len(np.unique(u))
 li = len(np.unique(i))
 # the good stuff
+
 interactions = csr_matrix((_ratings, (u, i)), shape=(lu, li))
 
-peek = pd.DataFrame(
-    interactions.todense(),
-    index=user_encoder.classes_,
-    columns=item_encoder.classes_
-)
+### train test split
+from lightfm.cross_validation import random_train_test_split
 
-peek.loc['07julia12'].sort_values(ascending=False)[:7]
+train, test = random_train_test_split(interactions, test_percentage=0.2)
 
-df[df['user'] == '07julia12']
+# model
 
-model = LightFM(loss='warp')
-model.fit(interactions, epochs=100)
-
-person = '07julia12'
-user_id = user_encoder.transform([person])[0]
-preds = model.predict(user_id, list(range(li)))
-
-pred_df = pd.DataFrame({
-    'product': item_encoder.classes_,
-    'rating': preds
-}).sort_values('rating', ascending=False)
-
-pred_df
-
-from lightfm.evaluation import precision_at_k
 from lightfm.evaluation import auc_score
 
-precision_at_k(model, interactions, k=10).mean()
-auc_score(model, interactions).mean()
+model = LightFM(loss='warp')
+
+auc = []
+for e in range(100):
+    model.fit_partial(train, epochs=1)
+    auc_train = auc_score(model, train).mean()
+    auc_test = auc_score(model, test).mean()
+    auc.append((auc_train, auc_test))
+auc = np.array(auc)
+
+from matplotlib import pyplot as plt
+
+plt.plot(auc[:, 0])
+plt.plot(auc[:, 1])
+
+
+model.predict(0, list(range(li)))
+
 
 reco = pred_df['product'].values.tolist()
 
