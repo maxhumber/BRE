9cd7f5802be3aeb9f68504a8d8e0f1978a643db5
Max Humber <max.humber@gmail.com>
2020-04-08 16:58:37 -0400

    clean up

--- 04B-app.py
+++ 04B-app.py
@@ -3,23 +3,23 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neighbors import NearestNeighbors
 from flask import Flask, request, render_template
 
-app = Flask(__name__, template_folder="templates_basic")
+app = Flask(__name__, template_folder="templates")
 
-with open("model/model.pkl", "rb") as f:
-    import dill
-    model = dill.load(f)
+with open("model.pkl", "rb") as f:
+    import cloudpickle
+    model = cloudpickle.load(f)
 
 @app.route("/")
 def index():
     repos = model.cv.get_feature_names()
-    random_repos = ", ".join(random.choices(repos, k=5))
-    return render_template("index.html", random_repos=random_repos)
+    random_candy = ", ".join(random.choices(repos, k=5))
+    return render_template("index.html", random_candy=random_candy)
 
 @app.route("/result", methods=["POST"])
 def predict():
-    repos = request.form["repos"]
-    repos = ",".join([r.strip() for r in repos.split(",")])
-    suggestions = model.predict([repos])[0]
+    candy = request.form["candy"]
+    candy = ",".join([c.strip() for c in candy.split(", ")])
+    suggestions = model.predict([candy])[0]
     random.shuffle(suggestions)
     return render_template("result.html", suggestions=suggestions[:5])
 
